612d604b9bada42726d4ceeb0e054be0d961d13c
aw478882601-hash <aw478882601@gmail.com>
2025-08-22 17:20:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1441,7 +1441,7 @@ bot.on('callback_query', async (ctx) => {
                 await ctx.answerCbQuery();
                 return ctx.reply("📝 أرسل أو وجّه الرسالة التالية:", { reply_markup: { force_reply: true } });
             }
-        }
+        
     } catch (error) {
         console.error("FATAL ERROR in callback_query handler:", error);
         console.error("Caused by callback_query data:", JSON.stringify(ctx.update.callback_query, null, 2));
